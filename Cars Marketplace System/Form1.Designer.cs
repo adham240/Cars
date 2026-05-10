11fd40b20cc0e2da65bc5a76cafa557a00b924b3
adham240 <adhammo80@yahoo.com>
2026-05-10 19:57:32 +0300

    Add files via upload

---
+++ Cars Marketplace System/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Cars_Marketplace_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            LOGIN = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            panel1 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.PORSCHE2;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(443, 746);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // LOGIN
+            // 
+            LOGIN.AutoSize = true;
+            LOGIN.Font = new Font("Algerian", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            LOGIN.ForeColor = SystemColors.InactiveCaption;
+            LOGIN.Location = new Point(574, 368);
+            LOGIN.Name = "LOGIN";
+            LOGIN.Size = new Size(158, 26);
+            LOGIN.TabIndex = 1;
+            LOGIN.Text = "Login Type:";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.ForeColor = Color.Black;
+            radioButton1.Location = new Point(94, 32);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(52, 19);
+            radioButton1.TabIndex = 2;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "USER";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.ForeColor = Color.Black;
+            radioButton2.Location = new Point(94, 76);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(84, 19);
+            radioButton2.TabIndex = 3;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "IT Manager";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ScrollBar;
+            panel1.BorderStyle = BorderStyle.Fixed3D;
+            panel1.Controls.Add(radioButton1);
+            panel1.Controls.Add(radioButton2);
+            panel1.Location = new Point(750, 307);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(273, 151);
+            panel1.TabIndex = 4;
+            panel1.Paint += panel1_Paint;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.WindowFrame;
+            ClientSize = new Size(1370, 747);
+            Controls.Add(panel1);
+            Controls.Add(LOGIN);
+            Controls.Add(pictureBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "CARS AUTO";
+            TopMost = true;
+            WindowState = FormWindowState.Maximized;
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label LOGIN;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Panel panel1;
+    }
+}
---
+++ Cars Marketplace System/Form1.cs
@@ -0,0 +1,34 @@
+using Cars_Marketplace_System.Properties;
+using System.Media;
+namespace Cars_Marketplace_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SoundPlayer startplayer = new SoundPlayer(Properties.Resources.StartPlayer);
+            startplayer.Play();
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
